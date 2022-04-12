9c4cb7a26a627f2e7a63bba89c9a28e0369b6ffa
João Silveira <joao.freixial.silveira@tecnico.ulisboa.pt>
2022-04-12 21:44:53 +0100

    improved actions

--- numbrix.py
+++ numbrix.py
@@ -6,11 +6,25 @@
 # 96915 Tomás Nunes
 # 95597 João Silveira
 
+from difflib import diff_bytes
 import sys
 from copy import deepcopy
 
-from matplotlib.pyplot import fill
+# from matplotlib.pyplot import fill
 from search import Problem, Node, astar_search, breadth_first_tree_search, depth_first_tree_search, greedy_search, recursive_best_first_search
+
+def consecutive_nones_indexes(x : list) -> list:
+    out = []
+    count = 0
+    for i in range(len(x)):
+        if x[i] == None:
+            count += 1
+        elif count:
+            out.append([i - count, i - 1])
+            count = 0
+    if count:
+        out.append([i - count + 1, i])
+    return out
 
 class NumbrixState:
     state_id = 0
@@ -28,12 +42,13 @@ class NumbrixState:
         Preenche um quadrado no tabuleiro
         """
 
-        state_copy = deepcopy(self)
-        state_copy.board.squares[row][col] = number
-        state_copy.board.numbers[number - 1] = (row, col)
-        state_copy.board.zeros = state_copy.board.zeros - 1
-        return state_copy
+        copy = deepcopy(self)
+        copy.board.squares[row][col] = number
+        copy.board.numbers[number - 1] = (row, col)
+        copy.board.zeros = copy.board.zeros - 1
+        copy.board.unused_segments = consecutive_nones_indexes(copy.board.numbers)
 
+        return copy
 
 class Board:
     def __init__(self, n: int, squares: list):
@@ -47,10 +62,14 @@ class Board:
         self.numbers = [None] * self.n_squares
         self.zeros = sum(map(lambda x: x == 0, [square for row in squares for square in row]))
 
+        # TODO: Melhorar
         for i in range(self.n):
             for j in range(self.n):
                 if squares[i][j] != 0:
                     self.numbers[self.squares[i][j] - 1] = (i, j)
+
+        self.unused_segments = consecutive_nones_indexes(self.numbers)
+
 
     def get_number(self, row: int, col: int) -> int:
         """
@@ -80,6 +99,11 @@ class Board:
         return (left, right)
 
     def get_neighbors(self, row : int, col : int):
+        """
+        Devolve os valores imediatamente à esquerda, direita,
+        cima e baixo respetivamente.
+        """
+
         left, right = self.adjacent_horizontal_numbers(row, col)
         down, up = self.adjacent_vertical_numbers(row, col)
         return (left, right, down, up)
@@ -117,72 +141,59 @@ class Numbrix(Problem):
         """
         Retorna uma lista de ações que podem ser executadas a
         partir do estado passado como argumento.
-        """
-        res = []
-        for i in range(state.board.n):
-            for j in range(state.board.n):
-                if state.board.squares[i][j] == 0:
-                    neighbors = state.board.get_neighbors(i, j)
-                    n_valid_neighbors = len([n for n in neighbors if n != None and n != 0])
-                    possible_actions_sq = [action for action in self.get_possible_values(i, j, state)]
-                    if n_valid_neighbors > 1 and possible_actions_sq == []:
-                        return []
-                    res += possible_actions_sq
-        return res
+        """ 
 
-    def get_possible_values(self, row: int, col: int, state: NumbrixState):
-        """
-        Returns a list of possible values for a given square.
-        This square's value must always satisfy at least 1 adjacent square.
-        """
+        def actions_for_value(chosen_number):
+            if chosen_number != 1 and state.board.numbers[chosen_number - 2] != None:
+                x1 = state.board.numbers[chosen_number - 2][0]
+                y1 = state.board.numbers[chosen_number - 2][1]
+                
+                left, right, down, up = state.board.get_neighbors(x1, y1)
+                if left == 0:
+                    yield (x1, y1 - 1, chosen_number)  
+                if right == 0:
+                    yield (x1, y1 + 1, chosen_number)  
+                if down == 0:
+                    yield (x1 + 1, y1, chosen_number)  
+                if up == 0:
+                    yield (x1 - 1, y1, chosen_number)
 
-        if state.board.squares[row][col] != 0:
-            return
+            if chosen_number != state.board.n_squares and state.board.numbers[chosen_number] != None:
+                x1 = state.board.numbers[chosen_number][0]
+                y1 = state.board.numbers[chosen_number][1]
 
-        neighbors = list(state.board.adjacent_vertical_numbers(row, col)) + list(state.board.adjacent_horizontal_numbers(row, col))
-        valid_neighbors = list(filter(lambda x: x != None and x != 0, neighbors))
+                left, right, down, up = state.board.get_neighbors(x1, y1)
+                if left == 0:
+                    yield (x1, y1 - 1, chosen_number)  
+                if right == 0:
+                    yield (x1, y1 + 1, chosen_number)  
+                if down == 0:
+                    yield (x1 + 1, y1, chosen_number)  
+                if up == 0:
+                    yield (x1 - 1, y1, chosen_number)
 
-        n_valid_neighbors = len(valid_neighbors)
-        n_edges = neighbors.count(None)
+        def select_next_number():
+            min_segment_size = float("inf")
+            for i in range(len(state.board.unused_segments)):
+                seg_size = state.board.unused_segments[i][1] - state.board.unused_segments[i][0]
+                if seg_size < min_segment_size:
+                    chosen_numbers = state.board.unused_segments[i]
+                    min_segment_size = seg_size
+            
+            left_candidate = chosen_numbers[0] + 1
+            right_candidate = chosen_numbers[1] + 1
 
-        if n_valid_neighbors == 0:
-            return
-
-        candidates = {}
-        for neighbor in valid_neighbors:
-            candidate_upper = neighbor + 1
-            candidate_lower = neighbor - 1
-            if candidate_upper < state.board.n_squares + 1 and state.board.numbers[candidate_upper - 1] == None:
-                candidates[candidate_upper] = candidates.get(candidate_upper, 0) + 1
-            if candidate_lower > 0 and state.board.numbers[candidate_lower - 1] == None:
-                candidates[candidate_lower] = candidates.get(candidate_lower, 0) + 1
-
-        if 1 in candidates.keys():
-            yield (row, col, 1)
-            del candidates[1]
-
-        if state.board.n_squares in candidates.keys():
-            yield (row, col, state.board.n_squares)
-            del candidates[state.board.n_squares]
-
-        if n_edges == 0:
-            if n_valid_neighbors <= 3:
-                for possible_value in candidates.items():
-                    yield (row, col, possible_value[0])
+            if min_segment_size == 0:
+                return left_candidate
+            elif left_candidate == 1:
+                return right_candidate
+            elif right_candidate == state.board.n_squares:
+                return left_candidate
             else:
-                for possible_value in filter(lambda x: x[1] == 2, candidates.items()):
-                    yield (row, col, possible_value[0])
-        elif n_edges == 1:
-            if n_valid_neighbors <= 2:
-                for possible_value in candidates.items():
-                    yield (row, col, possible_value[0])
-            else:
-                for possible_value in filter(lambda x: x[1] == 2, candidates.items()):
-                    yield (row, col, possible_value[0])
-        elif n_edges == 2:
-            for possible_value in filter(lambda x: x[1] == n_valid_neighbors, candidates.items()):
-                yield (row, col, possible_value[0])
+                return left_candidate
 
+        selected_number = select_next_number()
+        return [action for action in actions_for_value(selected_number)]
 
     def result(self, state: NumbrixState, action) -> NumbrixState:
         """
@@ -204,38 +215,38 @@ class Numbrix(Problem):
 
         if state.board.zeros != 0:
             return False
-        for i in range(1, state.board.n_squares):
+            
+        for i in range(state.board.n_squares - 1): 
             x1 = state.board.numbers[i][0]
             y1 = state.board.numbers[i][1]
-            x2 = state.board.numbers[i - 1][0]
-            y2 = state.board.numbers[i - 1][1]
+            x2 = state.board.numbers[i + 1][0]
+            y2 = state.board.numbers[i + 1][1]
+            
             man_distance = abs(x1 - x2) + abs(y1 - y2) - 1
             if man_distance > 1:
                 return False
+
         return True
 
-    # TODO: performance can be improved, iterate only on number gaps
     def h(self, node: Node):
         """
         Função heuristica utilizada para a procura A*.
         """
 
+        # TODO: performance can be improved, iterate only on number gaps
         filled_positions = [x for x in  node.state.board.numbers if x != None]
-
         for i in range(len(filled_positions) - 1):
             x1 = filled_positions[i][0]
-            x2 = filled_positions[i + 1][0]
             y1 = filled_positions[i][1]
+            x2 = filled_positions[i + 1][0]
             y2 = filled_positions[i + 1][1]
 
             num_distance = abs(node.state.board.squares[x1][y1] - node.state.board.squares[x2][y2])
-            if num_distance == 1:
-                continue
-
             man_distance = abs(x1 - x2) + abs(y1 - y2)
             if man_distance > num_distance:
                 return float("inf")
 
+        # Like an early goal test ??
         for i in range(len(filled_positions)):
             x = filled_positions[i][0]
             y = filled_positions[i][1]
@@ -255,16 +266,34 @@ class Numbrix(Problem):
                 elif n_zeros_neighbors == 0 and n_valid_neighbors < 2:
                     return float("inf")
 
+        # Completely surrounded zeros that are impossible to fill
+        for i in range(node.state.board.n):
+            for j in range(node.state.board.n):
+                if node.state.board.squares[i][j] == 0:
+                    neighbors = node.state.board.get_neighbors(i, j)
+                    if len(list(filter(lambda x : x != 0, neighbors))) == 4:
+                        num_neighbors = list(filter(lambda x : x != 0 and x != None, neighbors))
+                        n = len(num_neighbors)
+                        diff = float("inf")
+                        for k in range(n - 1):
+                            for l in range(k + 1, n):
+                                diff = min(abs(num_neighbors[k] - num_neighbors[l]), diff)
+                        if diff != 2:
+                            return float("inf")
+
         return node.state.board.zeros
 
 
 if __name__ == "__main__":
     board = Board.parse_instance(sys.argv[1])
-    print("Initial:\n", board.to_string(), sep="")
+    # print("Initial:\n", board.to_string(), sep="")
 
     problem = Numbrix(board)
     s0 = NumbrixState(board)
-    print(problem.actions(s0))
+    # print(problem.actions(s0))
+
+    # print(s0.board.numbers)
+    # print(s0.board.unused_segments)
 
     # s1 = problem.result(s0, (2, 2, 1))
     # s2 = problem.result(s1, (0, 2, 3))
@@ -276,12 +305,14 @@ if __name__ == "__main__":
 
     # goal_node = breadth_first_tree_search(problem)
     # goal_node = depth_first_tree_search(problem)
-    goal_node = greedy_search(problem, problem.h)
     # goal_node = recursive_best_first_search(problem, problem.h)
+
     # goal_node = astar_search(problem, problem.h, display=True)
+    goal_node = greedy_search(problem, problem.h)
 
     if goal_node != None:
-        print("Is goal?", problem.goal_test(goal_node.state))
-        print("Solution:\n", goal_node.state.board.to_string(), sep="")
+        # print("Is goal?", problem.goal_test(goal_node.state))
+        # print("Solution:\n", goal_node.state.board.to_string(), sep="")
+        print(goal_node.state.board.to_string())
     else:
         print("Found no solution!")
